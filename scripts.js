c80745eabb075dcd52987e0a5f122e2e901bf12a
Brady Klockenga and Maryana Antonyuk <brady@>
2019-03-25 13:09:10 -0700

    Add js file. BMI calculator, what is your name, age, fav food prompt.

---
+++ scripts.js
@@ -0,0 +1,59 @@
+var saySomething = function(whatToSay){
+  alert(whatToSay);
+}
+
+var Name = prompt("What is your name?")
+
+var Age = prompt("What is your age?")
+
+var Food = prompt("What is your favorite food?")
+
+saySomething("My Name is " + Name + "." + "I am " + Age + "." + "My Favorite food is" + Food + ".");
+
+var subtract = function(number1, number2){
+return number1 - number2;
+}
+
+var result = subtract(3,2);
+alert(result);
+
+var multiply = function(number1, number2){
+return number1 * number2;
+}
+var result = multiply(3,2);
+alert(result);
+var threeTimes = function (number1, number2, number3) {
+return number1 * number2 * number3;
+}
+var result = threeTimes(3,2,3);
+alert(result);
+var divide = function(number1, number2){
+return number1 / number2;
+}
+var result = divide(6,2);
+alert(result);
+var remainder = function(number1, number2){
+return number1 % number2;
+}
+var result = remainder(5,2);
+alert(result);
+
+// BMI calculator
+
+var inputtedHeight = prompt("What is your height?");
+
+ var multiply = function(inputtedHeight, inputtedHeight) {
+ return inputtedHeight * inputtedHeight;
+ }
+
+ var result1 = multiply(inputtedHeight, inputtedHeight);
+ alert(result1);
+
+ var inputtedWeight = prompt("What is your Weight?");
+
+ var divide = function(inputtedWeight, result1) {
+ return inputtedWeight / result1;
+ }
+
+  var result2 = divide(inputtedWeight, result1);
+ alert(result2);
